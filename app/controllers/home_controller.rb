699618a153cba197e90f5aacf28336af179a1db6
Joel Hogg <joelhogg45@gmail.com>
2024-05-20 17:53:08 +0100

    Redirects to bank working
    not completed what happens on the other side yet tho

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,26 @@ class HomeController < ApplicationController
   end
 
   def activate_bank
+    client = Client.create_client
     bank_id = params['bank_id'] #retreive bank id from button
     # Rails.logger.info("HERE--------------------------- #{bank_id}") #this works
+
+    #initalise a bank authorisation session
+    init = client.init_session(
+      # redirect url after successful authentication
+      redirect_url: "https://gocardless.com",
+      # institution id
+      institution_id: bank_id,
+      # a unique user ID of someone who's using your services, usually it's a UUID
+      reference_id: SecureRandom.uuid,
+      # A two-letter country code (ISO 639-1)
+      user_language: "en",
+      # option to enable account selection view for the end user
+      account_selection: false
+    )
+    link = init["link"] # bank authorization link
+    requisition_id = init["id"] # requisition id that is needed to get an account_id
+    Rails.logger.info("LINK------ #{link}")
+    redirect_to link, allow_other_host: true
   end
 end
